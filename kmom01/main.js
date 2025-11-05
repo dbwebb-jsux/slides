b3b11ec1d28ef928da5af503bddb491a29abd372
Emil Folino <emilfolino@gmail.com>
2025-11-05 10:51:28 +0100

    Done with kmom01

---
+++ kmom01/main.js
@@ -0,0 +1,27 @@
+let cakes = [
+    {
+        name: "Princesstårta",
+        price: 120,
+        amount: 10,
+    },
+    {
+        name: "Kanelbulle",
+        price: 22,
+        amount: 13,
+    },
+    {
+        name: "Mandelkubb",
+        price: 8,
+        amount: 20,
+    },
+    {
+        name: "Schackrutor",
+        price: 3,
+        amount: 98,
+    },
+    {
+        name: "Budapest-bakelse",
+        price: 35,
+        amount: 2,
+    },
+]
