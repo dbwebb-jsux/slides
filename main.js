ce2e008d4a045dc01b2093f94cf889d453a46b5b
atke24 <atke24@student.bth.se>
2025-11-29 14:17:41 +0100

    Webshop layout and product list

--- main.js
+++ main.js
@@ -1,2 +1,18 @@
-import slideorama from "https://cdn.jsdelivr.net/gh/emilfolino/slideorama@master/slideorama.min.js";
-slideorama.init("presentation", "slide-container");
+import auth from "./models/auth.js";
+
+async function renderProducts() {
+  const response = await fetch(`${auth.api_url}/products?api_key=${auth.api_key}`);
+  const result = await response.json();
+  const productList = document.getElementById("product-list");
+
+  const productArray = result.data.map((product) => {
+    return `<div class="product">
+  <img src="${product.image_url}" alt="Album art ${product.name}" />
+  <h3>${product.name}</h3>
+</div>`;
+  });
+
+  productList.innerHTML = productArray.join("\n");
+}
+
+renderProducts();
